4cf52c377da1ed86216aa24b0895e5781d5977c7
희은 <datakubit2@gmail.com>
2023-12-07 21:31:30 +0900

    refactor: CORS 문제 해결

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { typeOrmConfig } from 'typeOrm.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const categoryService = app.get(CategoryService);
   await categoryService.initializeCategories(); // 카테고리 초기화
   await app.listen(8080);
